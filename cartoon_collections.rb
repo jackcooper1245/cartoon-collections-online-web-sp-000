6bcb95a76b267dc1dc704a858e1d5a7d267e4599
Jack Cooper <jackcooper1245@gmail.com>
2020-04-08 11:54:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,6 @@ end
 def find_the_cheese(cheddar_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheddar_cheese.include do |cheese|
-    cheese == cheese_types
+  cheddar_cheese.include(cheese_types)
   end
 end

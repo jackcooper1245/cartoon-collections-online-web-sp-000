57bc0b729f5b4d20a4327edcd6fcac356f467bab
Jack Cooper <jackcooper1245@gmail.com>
2020-04-08 11:58:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,11 @@ def long_planeteer_calls(calls)
 end
 end
 
-def find_the_cheese(array)
-  array.find do |cheese|
-    
+def find_the_cheese(cheddar_cheese)
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheddar_cheese.find do |cheese|
+    cheese_types.include?
+   
+  end
 end
